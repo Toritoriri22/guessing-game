8d59f0adf3e9ad2a5bfd053017cc6abd781f76e9
Toritoriri <kuligina.vika@bk.ru>
2023-09-26 12:36:27 +0500

    feat: add solition

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.currentNumber = 0;
+        this.minNumber = 0;
+        this.maxNumber = 0;
+    }
 
     setRange(min, max) {
-
+        this.minNumber = min;
+        this.maxNumber = max;
     }
 
     guess() {
-
+        this.currentNumber = Math.ceil((this.minNumber + this.maxNumber) / 2);
+        return this.currentNumber;
     }
 
     lower() {
-
+        this.maxNumber = this.currentNumber;
     }
 
     greater() {
-
+        this.minNumber = this.currentNumber;
     }
 }
 
